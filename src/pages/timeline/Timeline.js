3b66f3619be7d6314c06e6707a5dff1b934372eb
VallariAg <val.agl002@gmail.com>
2020-11-18 03:36:42 +0530

    use Timeline from  @material-ui/lab

--- src/pages/timeline/Timeline.js
+++ src/pages/timeline/Timeline.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import './timeline.css';
 import { useQuery, gql } from '@apollo/client';
 import ReactMarkdown from 'react-markdown';
+import { TimelineDot, TimelineOppositeContent, Timeline, TimelineItem, TimelineSeparator, TimelineConnector, TimelineContent } from '@material-ui/lab';
+import Typography from '@material-ui/core/Typography';
+import useViewport from "../../hooks/useViewport"
+import './timeline.css';
 
 function MoreDesc(descButton, description) {
     if (descButton === "more" || descButton === "") {
@@ -15,16 +18,24 @@ function MoreDesc(descButton, description) {
         </p>
     }
 }
-function Block({ time, title, body, description }) {
+function Block({ time, title, body, description, isSmallView }) {
     const [descButton, setdescButton] = useState("more");
     // if (description === "") {
     //     setdescButton("");
     // }
-    return (
-        <li>
-            <div className="bullet pink"></div>
-            <div className="time">{time}</div>
-            <div className="body">
+    return (<TimelineItem className="timline-item">
+        {isSmallView ? "" :
+            <TimelineOppositeContent className="timline-time">
+                <Typography color="textSecondary" className="timline-time">{time}</Typography>
+            </TimelineOppositeContent>
+        }
+        <TimelineSeparator>
+            <TimelineDot />
+            <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>
+            <div>
+                {isSmallView ? <Typography color="textSecondary">{time}</Typography> : ""}
                 <h3>{title}</h3>
                 <h4>{body}</h4>
                 {MoreDesc(descButton, description)}
@@ -34,10 +45,11 @@ function Block({ time, title, body, description }) {
                     {descButton}
                 </button>
             </div>
-        </li>
-    )
+        </TimelineContent>
+    </TimelineItem>)
 }
-function Timeline() {
+function Timelines() {
+    let { width } = useViewport();
     const { loading, error, data } = useQuery(TimelineQuery)
 
     if (loading) return <p>Loading.. </p>
@@ -48,26 +60,23 @@ function Timeline() {
             <div className="timelineHeader">
                 <h1 className="pageHeader">Timeline </h1>
             </div>
-            <div class="container">
-                <div class="timeline">
-                    <ul>
-                        {
-                            data.allContent.map(({ id, title, body, time, description }) => {
-                                let date = new Date(parseInt(time));
-                                date = `${date.toLocaleString('default', { month: 'long' })} ${date.getUTCFullYear()}`
-                                // let date = new Date(parseInt(time)).toDateString();
-                                return <Block id={id} time={date} title={title} description={description} body={body} />
-                            })
-                        }
-                    </ul>
-                </div>
-            </div>
+            <Timeline align={width > 800 ? "alternate" : "left"}>
+                {
+                    data.allContent.map(({ id, title, body, time, description }) => {
+                        let date = new Date(parseInt(time));
+                        date = `${date.toLocaleString('default', { month: 'long' })} ${date.getUTCFullYear()}`
+                        // let date = new Date(parseInt(time)).toDateString();
+                        return <Block id={id} time={date} title={title} description={description} body={body} isSmallView={width < 800} />
+
+                    })
+                }
+            </Timeline>
         </div>
     )
 }
 
 
-export default Timeline;
+export default Timelines;
 
 
 const TimelineQuery = gql`
